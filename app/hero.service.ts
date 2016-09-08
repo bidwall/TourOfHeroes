c5c8535bfb80c0b4c19f4b5a14621d554d179a71
Palvinder Bidwall <pal@bidwall.co.uk>
2016-09-08 15:05:09 +0100

    Add & Delete a hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -26,11 +26,29 @@ export class HeroService {
 
     update(hero: Hero): Promise<Hero> {
         const url = `${this.heroesUrl}/${hero.id}`;
-        
+
         return this.http
                     .put(url, JSON.stringify(hero), {headers: this.headers})
                     .toPromise()
                     .then(() => hero)
+                    .catch(this.handleError);
+    }
+
+    create(name: string): Promise<Hero> {
+        return this.http
+                    .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+                    .toPromise()
+                    .then(res => res.json().data)
+                    .catch(this.handleError);
+    }
+
+    delete(id: number): Promise<void> {
+        let url = `${this.heroesUrl}/${id}`;
+
+        return this.http
+                    .delete(url, {headers: this.headers})
+                    .toPromise()
+                    .then(() => null)
                     .catch(this.handleError);
     }
 
